506c99f2dc775bb585cdb6e1b8aa4519380458df
YOGANANDH60 <yoga132005@gmail.com>
2025-08-19 10:47:42 +0530

    change in day1

--- DAY1/index.py
+++ DAY1/index.py
@@ -5,13 +5,12 @@ import matplotlib.pyplot as plt
 img = cv2.imread("summa.jpg")
 print(img.shape)
 
-img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+cv2.rectangle(img,(30,30),(150,150),(255,0,0),2)
+cropped = img[50:250,100:150]
+
+img = cv2.cvtColor(cropped, cv2.COLOR_BGR2RGB)
 
 
 plt.imshow(img)
 plt.axis('off')
 plt.show()
-
-
-
-
