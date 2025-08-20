9195338fdbfe33196fe68a89bb72d9e9aa9ac4e2
YOGANANDH60 <yoga132005@gmail.com>
2025-08-20 10:37:17 +0530

    some change and add missing code in day1

--- DAY1/index.py
+++ DAY1/index.py
@@ -4,13 +4,17 @@ import matplotlib.pyplot as plt
 
 img = cv2.imread("summa.jpg")
 print(img.shape)
+# rectangle
 
-cv2.rectangle(img,(30,30),(150,150),(255,0,0),2)
-cropped = img[50:250,100:150]
+# cv2.rectangle(img,(30,30),(150,150),(255,0,0),2)
+# cropped
+# cropped = img[50:250,100:150]
 
-img = cv2.cvtColor(cropped, cv2.COLOR_BGR2RGB)
 
 
-plt.imshow(img)
-plt.axis('off')
+h,w = img.shape[:2]
+m = cv2.getRotationMatrix2D((w//2,h//2),90,1)
+rotated = cv2.warpAffine(img,m,(h,w))
+plt.imshow(cv2.cvtColor(rotated,cv2.COLOR_BGR2RGB))
+plt.axis("off")
 plt.show()
