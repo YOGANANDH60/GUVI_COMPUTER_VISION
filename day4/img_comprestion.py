8115c2ab3eed914658285a345e26c030bab1e3b7
YOGANANDH60 <yoga132005@gmail.com>
2025-08-22 00:22:20 +0530

    andd practical 1 to 5

---
+++ day4/img_comprestion.py
@@ -0,0 +1,5 @@
+import cv2
+img = cv2.imread("building.jpg")
+cv2.imwrite("compressed_image.jpg",img,[cv2.IMWRITE_JPEG_QUALITY,50])
+cv2.imwrite("compressed_image.png",img,[cv2.IMWRITE_PNG_COMPRESSION,9])
+cv2.imwrite("compressed_image.webp",img,[cv2.IMWRITE_WEBP_QUALITY,50])
