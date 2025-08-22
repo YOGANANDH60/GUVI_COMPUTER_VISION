e0c8af243d9cd5691aa7997faec2afa7c18b062c
YOGANANDH60 <yoga132005@gmail.com>
2025-08-22 09:31:59 +0530

    add some change

--- day4/img_comprestion.py
+++ day4/img_comprestion.py
@@ -1,5 +1,5 @@
 import cv2
-img = cv2.imread("building.jpg")
+img = cv2.imread("summa.jpg")
 cv2.imwrite("compressed_image.jpg",img,[cv2.IMWRITE_JPEG_QUALITY,50])
 cv2.imwrite("compressed_image.png",img,[cv2.IMWRITE_PNG_COMPRESSION,9])
 cv2.imwrite("compressed_image.webp",img,[cv2.IMWRITE_WEBP_QUALITY,50])
